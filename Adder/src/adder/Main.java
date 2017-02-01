5e8c083771cf2c7de52be5d456c064df23a9f6b1
Amanda Schlarman <amanda.schlarman@gmail.com>
2017-02-01 00:08:53 -0600

    Question 13 Homework 1
    Final commit

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -4,14 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+            if (args.length < 3 || args.length > 3){
+                throw  InvalidLength;
+            }
+            else {
+                if (args[0] != "-" || (args[i] > "0" && args[i] < "9")) {
+                    throw InvalidCharacter;
+                } else {
+                    for (int i = 1; i < args.length; i++) {
+                        if (args[i] > "0" && args[i] < "9")
+                                else{
+                            throw InvalidCharacter;
+                        }
+                    }
+                }
+            }
             int result = addArguments(args);
             System.out.println(result);
-        } catch (Exception e) {
-            System.err.println("Please provide three integers to add");
+        } catch (InvalidCharacter e) {
+            System.err.println("Invalid Character");
+        }
+        catch (InvalidLength e) {
+            System.err.println("Please enter 3 arguemnts");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[2]);
+        return Integer.valueOf(args[0]) + Integer.valueOf(args[1] + Integer.valueOf(args[2]);
     }
 }
